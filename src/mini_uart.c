3d33d702e74cb42ecd34d9e5ca9969edaa45e184
TJ-Albertson <THOMASJOHNNY77@GMAIL.COM>
2023-08-02 17:26:55 -0500

    added spaces

--- src/mini_uart.c
+++ src/mini_uart.c
@@ -42,16 +42,16 @@ void uart_init ( void )
 	delay(150);
 	put32(GPPUDCLK0,(1<<14)|(1<<15));
 	delay(150);
-	put32(GPPUDCLK0,0);
+	put32(GPPUDCLK0, 0);
 
-	put32(AUX_ENABLES,1);                   // Enable mini uart (this also enables access to its registers)
-	put32(AUX_MU_CNTL_REG,0);               // Disable auto flow control and disable receiver and transmitter (for now)
-	put32(AUX_MU_IER_REG,0);                // Disable receive and transmit interrupts
-	put32(AUX_MU_LCR_REG,3);                // Enable 8 bit mode
-	put32(AUX_MU_MCR_REG,0);                // Set RTS line to be always high
-	put32(AUX_MU_BAUD_REG,270);             // Set baud rate to 115200
+	put32(AUX_ENABLES ,1);                   // Enable mini uart (this also enables access to its registers)
+	put32(AUX_MU_CNTL_REG, 0);               // Disable auto flow control and disable receiver and transmitter (for now)
+	put32(AUX_MU_IER_REG, 0);                // Disable receive and transmit interrupts
+	put32(AUX_MU_LCR_REG, 3);                // Enable 8 bit mode
+	put32(AUX_MU_MCR_REG, 0);                // Set RTS line to be always high
+	put32(AUX_MU_BAUD_REG, 270);             // Set baud rate to 115200
 
-	put32(AUX_MU_CNTL_REG,3);               // Enable transmitter and receiver
+	put32(AUX_MU_CNTL_REG, 3);               // Enable transmitter and receiver
 }
 
 // This function is required by printf function
