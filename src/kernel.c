18930304a89afac304453a2dbd10c41a428b9542
TJ-Albertson <THOMASJOHNNY77@GMAIL.COM>
2023-08-06 20:40:09 -0500

    add exception level printf

--- src/kernel.c
+++ src/kernel.c
@@ -6,12 +6,23 @@
 
 void kernel_main(void)
 {
+
+	
+
+
 	uart_init();
 	init_printf(0, put_character);
+
+	int el = get_el();
+	printf("Exception level: %d \r\n", el);
+
 	irq_vector_init();
 	timer_init();
 	enable_interrupt_controller();
 	enable_irq();
+
+	el = get_el();
+	printf("Exception level: %d \r\n", el);
 
 	while (1){
 		uart_send_character(uart_recv());
